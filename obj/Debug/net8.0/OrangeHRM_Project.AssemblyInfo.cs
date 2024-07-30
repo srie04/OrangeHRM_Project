9de482fc1cbe4a4cb51c4a8df828a232e00c4b37
srie04 <sristark42@gmail.com>
2024-07-30 22:06:00 +0530

    PIM

--- obj/Debug/net8.0/OrangeHRM_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/OrangeHRM_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrangeHRM_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891bdb8601ce6d048bb9dcac405adc9b3796c170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b245fcec58d1ce663021003f1e5175e1a0872c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrangeHRM_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrangeHRM_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ utilities/pages/PIM_Configuration_CustomFields.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeHRM_Project.utilities.pages
+{
+    internal class PIM_Configuration_CustomFields : BaseClass
+    {
+        [FindsBy(How = How.XPath, Using = "//button[normalize-space()='Add']")]
+        private IWebElement addBtn;
+
+        [FindsBy(How = How.XPath, Using = "(//div/input)[2]")]
+        private IWebElement fieldTextBox;
+
+        [FindsBy(How = How.CssSelector, Using = ".oxd-select-text--after")]
+        private IWebElement screenDropdownBtn;
+
+        [FindsBy(How = How.XPath, Using = "(//div[@class='oxd-select-text--after'])[2]")]
+        private IWebElement typeDropdownBtn;
+
+        [FindsBy(How = How.XPath, Using = "//button[normalize-space()='Add']")]
+        private IWebElement addBtn;
+
+    }
+}
