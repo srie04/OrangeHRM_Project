0ea00f67d8b3fe983b2b9510a573e4aad84729b2
unknown <mathusuthanan1030@gmail.com>
2024-07-30 22:30:04 +0530

    ss

---
+++ test/Admin_Job_Titles_Test.cs
@@ -0,0 +1,39 @@
+﻿using OrangeHRM_Project.utilities;
+using OrangeHRM_Project.utilities.pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeHRM_Project.test
+{
+    internal class Admin_Job_Titles_Test
+    {
+        [SetUp]
+        public void setup()
+        {
+            Login login = new Login();
+            login.performLogin("Admin", "admin123");
+
+        }
+
+        [Test]
+
+        public void jobTitletest()
+        {
+            Admin_Job_Title job = new Admin_Job_Title();
+
+            job.addjobtitle();
+        }
+
+        [TearDown]
+
+        public void tearDown()
+        {
+
+            BaseClass.closeBrowser();
+        }
+
+    }
+}
---
+++ utilities/pages/Admin_Job_Title.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeHRM_Project.utilities.pages
+{
+    internal class Admin_Job_Title : BaseClass
+    {
+        [FindsBy(How = How.XPath, Using = "//span[text()='Admin']/..")]
+        private IWebElement admin;
+        [FindsBy(How = How.XPath, Using = "//span[text()='Job ']")]
+        private IWebElement job;
+        [FindsBy(How = How.XPath, Using = "//li/a[text()='Job Titles']")]
+        private IWebElement title;
+        [FindsBy(How = How.XPath, Using = "//div[@class='oxd-layout-context']/div/div/div/div/button")]
+        private IWebElement button;
+        [FindsBy(How = How.XPath, Using = "//h6[text()='Job Titles']")]
+        private IWebElement titlss;
+        //div[@class='oxd-layout-context']/div/div/div/div/button
+        //h6[text()='Job Titles']
+        public Admin_Job_Title()
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void addjobtitle()
+        {
+            admin.Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(job));
+            job.Click();
+            title.Click();
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(titlss));
+            //Thread.Sleep(4000);
+            button.Click();
+        }
+
+        public void cancel()
+        {
+            closeBrowser();
+        }
+    }
+}
+
